fa20090e4a1ae4f46491cde69da6e52faf197bc1
afavia <afavia.student@manhattan.edu>
2011-02-08 12:19:33 -0500

    node 2nd commit

--- src/Node.java
+++ src/Node.java
@@ -1,12 +1,14 @@
 import java.util.Vector;
 
+enum NODE_TYPE { FUNCTION, OBJECT };
 
 public class Node {
 	Vector<Node> neighbors;
+	NODE_TYPE thisType;
 	
-	public Node(){
+	
+	public Node(NODE_TYPE thisType){
 		neighbors = new Vector<Node>();
-	}
-		
-	
+		this.thisType = thisType;
+	}		
 }
