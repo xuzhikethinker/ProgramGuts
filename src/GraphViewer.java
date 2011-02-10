033fc10aa3c7bab2b80425382f95560bdc7cb933
Anthony Favia <affavia@gmail.com>
2011-02-09 19:53:59 -0500

    updated drawer class

--- src/GraphViewer.java
+++ src/GraphViewer.java
@@ -4,6 +4,8 @@ import java.util.Vector;
  * This is where the graphics guys will put all of their code.
  * Can make more classes but have everything return to here.
  * 
+ * Need to have a window that draws a canvas
+ * 
  * Goal for first revision is to be able to draw a basic graph
  * 
  * Functions should be represented by a rectangles and objects are circles
