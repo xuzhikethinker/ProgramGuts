9116dc98c09f64866bc9407436d9acb8f85816ae
afavia <afavia.student@manhattan.edu>
2011-03-03 11:37:10 -0500

    changed to vecotr of nodes

--- src/BuilderDebugger.java
+++ src/BuilderDebugger.java
@@ -71,8 +71,8 @@ public class BuilderDebugger {
 	 * 
 	 * @param name
 	 */
-	public void AddObjectNode(String name) {
-		ObjectNode newObject = new ObjectNode(name);
+	public void AddObjectNode(String name, String value) {
+		ObjectNode newObject = new ObjectNode(name, value);
 
 		progGraph.add(newObject);
 	}
@@ -83,16 +83,16 @@ public class BuilderDebugger {
 	 * @param from
 	 * @param to
 	 */
-	public void AddObjectNodeConnection(ObjectNode from, ObjectNode to, String edgeName) {
+	public void AddObjectNodeConnection(ObjectNode from, ObjectNode to) {
 		//to.ObjectsConnectedTo.add(from);
-		from.ObjectsConnectedTo.put(edgeName, to);
+		from.ObjectsConnectedTo.add(to);
 	}
 
 	/**
 	 * can use this func to add more function nodes to the graph
 	 */
-	public void AddFunctionNode(FunctionNode calledFrom, String name) {
-		FunctionNode newFunction = new FunctionNode(calledFrom, name,
+	public void AddFunctionNode(FunctionNode calledFrom, String name, String value) {
+		FunctionNode newFunction = new FunctionNode(calledFrom, name, value,
 				calledFrom.stackPosition + 1);
 
 		progGraph.add(newFunction);
@@ -105,8 +105,8 @@ public class BuilderDebugger {
 	 * @param to
 	 */
 	public void AddFunctionToObjectNodeConnection(FunctionNode from,
-			ObjectNode to, String edgeName) {
-		from.ObjectsConnectedTo.put(edgeName, to);
+			ObjectNode to) {
+		from.ObjectsConnectedTo.add(to);
 	}
 	
 	public static int findPrevFuncNode( Vector<Node> g ) {
