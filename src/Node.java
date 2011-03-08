102f8276058215018ff451e7db0839c6491acf57
afavia <afavia.student@manhattan.edu>
2011-03-08 13:22:47 -0800

    Inserted hashtable to represent ndoes connected to eachother and fixed output a little

--- src/Node.java
+++ src/Node.java
@@ -5,7 +5,45 @@ import java.util.Vector;
 enum NODE_TYPE { FUNCTION, OBJECT };
 
 /**
+ * general node classimport java.util.Hashtable;
+import java.util.Vector;
+
+
+enum NODE_TYPE { FUNCTION, OBJECT };
+
+/**
  * general node class
+ */
+public class Node {
+	
+    int xCord, yCord; //stackPosition of node on canvas
+    int vertexID;
+    
+    String nodeType;
+    String name;
+    String value;
+    
+    int stackPosition;
+    
+
+    Vector<Node> ObjectsConnectedTo = new Vector<Node>();
+    /**
+     * generic constructor for node class
+     */
+    public Node() {
+    }
+
+    /**
+     * Adds a connection between this node and "connectedTo" by 
+     * adding an entry into the hashtable
+     * @param edgeName
+     * @param connectedTo
+     */
+    public void addConnection(Node connectedTo) {
+        ObjectsConnectedTo.add(connectedTo);
+    }
+	
+}
  */
 public class Node {
 	
